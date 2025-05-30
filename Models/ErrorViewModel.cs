46d6591db7d6eff975c9601762571899b6edf4e0
semaa <semaa@DESKTOP-LE4OU4A>
2025-05-30 18:54:14 +0300

    ASP.NET Core MVC yapısı kuruldu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _blog_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
