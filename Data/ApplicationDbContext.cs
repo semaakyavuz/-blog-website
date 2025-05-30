d5b2a10c05c93fe121a3da60d2fee3f7f66925c7
semaa <semaa@DESKTOP-LE4OU4A>
2025-05-30 19:56:44 +0300

    EF Core: User modeli, DbContext ve SQLite bağlantısı eklendi

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using _blog_website.Models;
+
+namespace _blog_website.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace _blog_website.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+}
